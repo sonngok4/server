704d053579fdc53af680a0839f7b65ab4cf4839a
sonngok4 <ngoson2k40501@gmail.com>
2025-04-07 05:10:48 +0700

    change access db privacy

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(debugMiddleware);
+// app.use(debugMiddleware);
 
 app.use('/api/auth', authRouter);
 app.use('/api/admin', adminRouter);
