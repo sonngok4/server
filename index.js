83f567b024a553cb8802d69d9fd2edb7728bd213
sonngok4 <ngoson2k40501@gmail.com>
2025-04-15 12:41:06 +0700

    fix upload size

--- index.js
+++ index.js
@@ -53,8 +53,9 @@ app.use(
 	}),
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Increase payload size limit
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(debugMiddleware);
 
